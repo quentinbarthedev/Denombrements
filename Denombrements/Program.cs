591f81990506e37f8ff6aa2516f2a8060e680251
Quentinbarthedev <Quentinbarthe.dev@gmail.com>
2026-01-02 15:56:31 -0700

    Optimisation du code

--- Denombrements/Program.cs
+++ Denombrements/Program.cs
@@ -1,25 +1,49 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Denombrements
 {
     class Program
     {
+        static void menu(ref int choix)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Permutation ...................... 1");
+            Console.WriteLine("Arrangement ...................... 2");
+            Console.WriteLine("Combinaison ...................... 3");
+            Console.WriteLine("Quitter .......................... 0");
+            Console.Write("Choix :                            ");
+            choix = int.Parse(Console.ReadLine());
+        }
+        static long saisie1(int n, long r2)
+        {
+            for (int k = 1; k <= n; k++)
+            {
+                r2 *= k;
+            }
+            return r2;
+        }
+        static long saisie2(string message1, string message2, ref int t, ref int n, long r1)
+        {
+            Console.Write(message1 + " = ");
+            t = int.Parse(Console.ReadLine());
+            Console.Write(message2 + " = ");
+            n = int.Parse(Console.ReadLine());
+            // calcul de r1
+            r1 = 1;
+            for (int k = (t - n + 1); k <= t; k++)
+            {
+                r1 *= k;
+            }
+            return r1;
+        }
         static void Main(string[] args)
         {
-            int choix = 1;
+            int n = 0, t = 0, choix = 1;
+            long r1 = 1, r2 = 1;
             while (choix != 0)
             {
-                Console.WriteLine("Permutation ...................... 1");
-                Console.WriteLine("Arrangement ...................... 2");
-                Console.WriteLine("Combinaison ...................... 3");
-                Console.WriteLine("Quitter .......................... 0");
-                Console.Write("Choix :                            ");
-                choix = int.Parse(Console.ReadLine());
-
+                menu(ref choix);
+                // Choix possibles
                 switch (choix)
                 {
                     case 0:
@@ -29,76 +53,39 @@ namespace Denombrements
                         break;
                     case 1:
                         {
-                            // saisir le nombre
+                            // saisie du nombre d'éléments à gérer
                             Console.Write("nombre total d'éléments à gérer = ");
-                            int nombre = int.Parse(Console.ReadLine());
-                            // calcul de r
-                            long taille1 = 1;
-                            for (int k = 1; k <= nombre; k++)
-                            {
-                                taille1 *= k;
-                            }
-                            Console.WriteLine(nombre + "! = " + taille1);
+                            n = int.Parse(Console.ReadLine());
+                            // calcul de r2
+                            r2 = saisie1(n, 1);
+                            // Affichage
+                            Console.WriteLine(n + "! = " + r2);
                         }
                         break;
                     case 2:
                         {
-                            // saisir le nombre
-                            Console.Write("nombre total d'éléments à gérer = ");
-                            int total = int.Parse(Console.ReadLine());
-                            // saisir le nombre de sous-ensembles
-                            Console.Write("nombre d'éléments dans le sous ensemble = ");
-                            int nombre = int.Parse(Console.ReadLine());
-                            // calcul de r
-                            long taille2 = 1;
-                            for (int k = (total - nombre + 1); k <= total; k++)
-                            {
-                                taille2 *= k;
-                            }
-                            //Console.WriteLine("résultat = " + (r1 / r2));
-                            Console.WriteLine("A(" + total + "/" + nombre + ") = " + taille2);
+                            // saisie du nombre d'éléments et sous ensembles à gérer
+                            r1 = saisie2("nombre total d'éléments à gérer", "nombre d'éléments dans le sous ensemble ", ref t, ref n, r1);                         
+                            // Affichage
+                            Console.WriteLine("A(" + t + "/" + n + ") = " + r1);
                         }
                         break;
                     case 3:
                         {
-                            // saisir le nombre d'éléments
-                            Console.Write("nombre total d'éléments à gérer = ");
-                            int total = int.Parse(Console.ReadLine());
-                            // saisir le nombre de sous-ensembles
-                            Console.Write("nombre d'éléments dans le sous ensemble = ");
-                            int nombre = int.Parse(Console.ReadLine());
-                            // calcul de r1
-                            long taille2 = 1;
-                            for (int k = (total - nombre + 1); k <= total; k++)
-                            {
-                                taille2 *= k;
-                            }
+                            // saisie du nombre d'éléments et sous ensembles à gérer
+                            r1 = saisie2("nombre total d'éléments à gérer", "nombre d'éléments dans le sous ensemble ", ref t, ref n, r1);
                             // calcul de r2
-                            long taille1 = 1;
-                            for (int k = 1; k <= nombre; k++)
-                            { 
-                                taille1 *= k;
-                            }
-                            // calcul de r3
-                            Console.WriteLine("C(" + total + "/" + nombre + ") = " + (taille2 / taille1));
+                            r2 = saisie1(n, 1);
+                            // Calcul de r3 et affichage
+                            Console.WriteLine("C(" + t + "/" + n + ") = " + (r1 / r2));
                         }
                         break;
                     default:
-                        {
-                            do
-                            {
-                                Console.WriteLine("Erreur: veuillez choisir parmi 0, 1, 2, et 3");
-                                Console.WriteLine("Permutation ...................... 1");
-                                Console.WriteLine("Arrangement ...................... 2");
-                                Console.WriteLine("Combinaison ...................... 3");
-                                Console.WriteLine("Quitter .......................... 0");
-                                Console.Write("Choix :                            ");
-                                choix = int.Parse(Console.ReadLine());
-                            } while (choix != 0 && choix != 1 & choix != 2 && choix != 3);
-
+                        {                         
+                            Console.WriteLine("Erreur : choisissez entre 0, 1, 2, ou 3");                         
                         }
                         break;
-                }               
+                }
             }
             Console.ReadLine();
         }
